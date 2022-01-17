de5613a3602e4d13d06d459954785bd229d35a79
MoSangIl <mosang1230@naver.com>
2022-01-17 21:53:28 +0900

    Function and Parameter Optional

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "SangIl", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hi, I'm ${name}, ${age}, and ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello")
+const name = "SangIl",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hi, I'm ${name}, ${age}, and ${gender}`);
+};
+
+sayHi(name, age);
+
+// gender is optional.. 
+
+export {};
