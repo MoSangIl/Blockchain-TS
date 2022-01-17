6dd4776dcf67a8f6cfce828e1606fb59018e9aab
MoSangIl <mosang1230@naver.com>
2022-01-17 22:16:16 +0900

    Add tsc-watch development tool

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "SangIl", age = 24, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hi, I'm ${name}, ${age}, and ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
